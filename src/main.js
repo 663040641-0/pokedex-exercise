ec4b6a17f2ea77bb21e693114777bf9ef3f2c04e
663040641-0 <151108329+663040641-0@users.noreply.github.com>
2025-05-08 19:48:10 +0700

    base HTML CSS commit

--- src/main.js
+++ src/main.js
@@ -1,24 +1,67 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const tabs = document.querySelectorAll('.tab-button');
+let selectedGen = 1;
 
-setupCounter(document.querySelector('#counter'))
+const  card = document.querySelector(".card");
+const dialog = document.querySelector(".myDialog");
+const dialogExit = document.querySelector("#dialog-exit");
+const infoButton = document.querySelectorAll(".info-button");
+let selectedData = "about";
+const about =
+    `<h1>Species - </h1>
+    <h1>Height - </h1>
+    <h1>Weight - </h1>
+    <h1>Abilities - </h1>
+    `;
+const baseStat =
+    `<h1>HP - </h1>
+    <h1>Attack - </h1>
+    <h1>Defense - </h1>
+    <h1>Sp.Atk - </h1>
+    <h1>Sp.Def - </h1>
+    <h1>Speed - </h1>
+    <h1>Total - </h1>
+    `;
+const dialogInfo = document.querySelector(".dialog-info");
+
+//gen selected
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        tabs.forEach(t => {
+            t.classList.remove('border-b-2', 'border-blue-500', 'font-bold', 'bg-white');
+        });
+        tab.classList.add('border-b-2', 'border-blue-500', 'font-bold', 'bg-white');
+
+        selectedGen = tab.getAttribute('data-gen');
+        console.log('Selected Generation:', selectedGen);
+    });
+});
+
+//dialog
+card.addEventListener('click', () => {
+    card.classList.remove('card')
+    card.classList.add('card-clicked');
+    dialog.showModal();
+});
+
+dialogExit.addEventListener('click', () => {
+    dialog.close();
+})
+dialogInfo.innerHTML = about;
+
+//dialog About and Base stats
+infoButton.forEach(but => {
+    but.addEventListener('click', () => {
+        but.classList.add('bg-gray-500');
+
+        selectedData = but.getAttribute('value');
+        console.log('Now you see', selectedData);
+        if (selectedData === 'baseStat') {
+            dialogInfo.innerHTML = baseStat;
+        }
+        else  {
+            dialogInfo.innerHTML = about;
+        }
+    });
+});
