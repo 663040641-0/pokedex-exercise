84de5324b65389e7000c4b97b29ed97952e14e27
663040641-0 <151108329+663040641-0@users.noreply.github.com>
2025-05-09 14:23:16 +0700

    style type element added

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import './style.css'
+import generations from "./data/generation.js";
 
+let pageOps = '';
 const tabs = document.querySelectorAll('.tab-button');
 let selectedGen = 1;
+const pageOp = document.querySelectorAll('.page-button');
+const itemPerPage = document.querySelector('#itemPerPage');
+let itemsPerPage = 10;
 
 const  card = document.querySelector(".card");
 const dialog = document.querySelector(".myDialog");
-const dialogExit = document.querySelector("#dialog-exit");
+const dialogExit = document.querySelector(".dialog-exit");
 const infoButton = document.querySelectorAll(".info-button");
 let selectedData = "about";
 const about =
@@ -25,6 +30,8 @@ const baseStat =
     `;
 const dialogInfo = document.querySelector(".dialog-info");
 
+let limitPage = 10;
+
 //gen selected
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
@@ -38,30 +45,61 @@ tabs.forEach(tab => {
     });
 });
 
+//item display per page
+itemPerPage.addEventListener('change', (e) => {
+    itemsPerPage = itemPerPage.value;
+    console.log(itemsPerPage);
+})
+
+//previous page and next page
+pageOp.forEach(pageBut => {
+    pageBut.addEventListener('click', (e) => {
+        if (pageBut.value === 'pre') {
+            console.log(`${pageBut.value} clicked`);
+        }
+        else {
+            console.log(`${pageBut.value} clicked`);
+        }
+    })
+})
+
 //dialog
 card.addEventListener('click', () => {
-    card.classList.remove('card')
-    card.classList.add('card-clicked');
     dialog.showModal();
 });
 
 dialogExit.addEventListener('click', () => {
     dialog.close();
 })
+
+dialog.addEventListener('click', (e) => {
+    const rect = dialog.getBoundingClientRect();
+    const isInDialog =
+        rect.top <= e.clientY &&
+        e.clientY <= rect.top + rect.height &&
+        rect.left <= e.clientX &&
+        e.clientX <= rect.left + rect.width;
+    if (!isInDialog) {
+        dialog.close();
+    }
+});
+
 dialogInfo.innerHTML = about;
 
 //dialog About and Base stats
 infoButton.forEach(but => {
     but.addEventListener('click', () => {
-        but.classList.add('bg-gray-500');
+        infoButton.forEach(b => {
+            b.classList.remove('border-b-2', 'border-blue-500', 'font-bold', 'bg-white');
+        });
+        but.classList.add('border-b-2', 'border-blue-500', 'font-bold', 'bg-white');
 
         selectedData = but.getAttribute('value');
-        console.log('Now you see', selectedData);
         if (selectedData === 'baseStat') {
             dialogInfo.innerHTML = baseStat;
-        }
-        else  {
+        } else {
             dialogInfo.innerHTML = about;
         }
     });
 });
+
